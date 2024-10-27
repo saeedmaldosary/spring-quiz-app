5d62040acd83f6565b9905c878533d8ea4989564
Saeed Aldosary <saldosary@scai.sa>
2024-10-27 11:52:19 +0300

    Find questions by category

--- src/main/java/com/saeedmaldosary/quizapp/controller/QuestionController.java
+++ src/main/java/com/saeedmaldosary/quizapp/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.saeedmaldosary.quizapp.Question;
 import com.saeedmaldosary.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,11 @@ public class QuestionController {
     public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category) {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+
 }
